662949e56ca8de406290351609aa930c2298ff56
Jon Tuite <jon.tuite@gmail.com>
2020-10-06 10:24:39 -0700

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID "Tuton House"
-#define SECRET_PASS "Hannington"
-#define SECRET_KEY "05668621-3a4a-4dac-9423-14582e748d05"
+#define SECRET_SSID "<YOUR SSID HERE>"
+#define SECRET_PASS "<YOUR WIFI PASSORD HERE>"
+#define SECRET_KEY "<YOUR HEC TOKEN HERE>"
